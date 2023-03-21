da325ece1d8b997d2a8dd63b005dc639ff01d274
Olya2806 <115026086+Olya2806@users.noreply.github.com>
2023-03-21 15:02:18 +0000

    Deploying to gh-pages from @ Olya2806/goit-js-hw-09@3cd9ff7adca9587a1048d034cb1de6848a0e1f07 🚀

--- 01-color-switcher.967b4be5.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");e.addEventListener("click",(function(){clearInterval(changeColorTimer),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)})),e.setAttribute("disabled",!1),t.addEventListener("click",(()=>{changeColorTimer=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.removeAttribute("disabled"),t.setAttribute("disabled",!0)}));
-//# sourceMappingURL=01-color-switcher.967b4be5.js.map
