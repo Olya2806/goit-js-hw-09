91196e9f12e98eb6d3608624d3d4507f83389ad5
Olya2806 <115026086+Olya2806@users.noreply.github.com>
2023-03-21 20:22:23 +0200

    3 завдання

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,39 +1,47 @@
 import Notiflix from 'notiflix';
 
-// const delayInput = document.querySelector('input[name="delay"]');
-// const delayStepInput = document.querySelector('input[name="step"]');
-// const promisesAmount = document.querySelector('input[name="amount"]');
+const delayInput = document.querySelector('input[name="delay"]');
+const delayStepInput = document.querySelector('input[name="step"]');
+const promisesAmount = document.querySelector('input[name="amount"]');
 const form = document.querySelector('.form');
 form.addEventListener('submit', onFormSubmit);
 
+ const delay = Number(delayInput.value);
+  const step = Number(delayStepInput.value);
+  const amount = Number(promisesAmount.value);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+ return new Promise((resolve, reject) => {
+setTimeout(() => {
+const shouldResolve = Math.random() > 0.3;
+if (shouldResolve) {
+resolve({ position, delay });
+} else {
+reject({ position, delay });
+}
+}, delay);
+});
+}
+
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  for (let num = 0; num <= amount; num + 1) {
+    
+    createPromise(num + 1, delay + num * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    createPromise(position, delay)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-    // Fulfill
-  } else {
-    createPromise(position, delay)
-    .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-    // Reject
-  }
-}
 
-createPromise(position + 1, delayInput.value + delayInput.value)
 
-function onFormSubmit() {
-  
-}
+
+
+
+
