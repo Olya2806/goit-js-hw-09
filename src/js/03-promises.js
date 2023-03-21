a83195ce5cce9d47f111e404df6ff9217e64af6e
Olya2806 <115026086+Olya2806@users.noreply.github.com>
2023-03-21 16:31:28 +0200

    jhgf

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,11 @@
 import Notiflix from 'notiflix';
 
+// const delayInput = document.querySelector('input[name="delay"]');
+// const delayStepInput = document.querySelector('input[name="step"]');
+// const promisesAmount = document.querySelector('input[name="amount"]');
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -7,4 +13,27 @@ function createPromise(position, delay) {
   } else {
     // Reject
   }
+}
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    createPromise(position, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+    // Fulfill
+  } else {
+    createPromise(position, delay)
+    .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    // Reject
+  }
+}
+
+createPromise(position + 1, delayInput.value + delayInput.value)
+
+function onFormSubmit() {
+  
 }
