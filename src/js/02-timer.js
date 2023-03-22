4a97fd34911466ecc563ee32bb11f7d1cc866f19
Olya2806 <115026086+Olya2806@users.noreply.github.com>
2023-03-22 21:36:55 +0200

    додає перевірку на закінчення часу

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,14 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
 const usersDateInput = document.querySelector('#datetime-picker');
-const startTimerButton = document.querySelector('button[data-start]')
- const daysField = document.querySelector("[data-days]");
-  const hoursField = document.querySelector("[data-hours]");
-  const minutesField = document.querySelector("[data-minutes]");
-  const secondsField = document.querySelector("[data-seconds]");
-startTimerButton.setAttribute('disabled', false)
+const startTimerButton = document.querySelector('button[data-start]');
+const daysField = document.querySelector('[data-days]');
+const hoursField = document.querySelector('[data-hours]');
+const minutesField = document.querySelector('[data-minutes]');
+const secondsField = document.querySelector('[data-seconds]');
+startTimerButton.setAttribute('disabled', false);
 
 let selectedDate = 0;
 
@@ -17,36 +17,40 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0], new Date());
-      
-      if (selectedDates[0] >= new Date()) {
-          startTimerButton.removeAttribute('disabled')
-          selectedDate = selectedDates[0]
-           
-      } else {
-          Notiflix.Notify.failure("Please choose a date in the future");
-        //   startTimerButton.setAttribute('disabled', true)
-      }
+  onClose(selectedDates) {
+    console.log(selectedDates[0], new Date());
+
+    if (selectedDates[0] >= new Date()) {
+      startTimerButton.removeAttribute('disabled');
+      selectedDate = selectedDates[0];
+    } else {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      //   startTimerButton.setAttribute('disabled', true)
+    }
   },
 };
 flatpickr(usersDateInput, options);
 
-startTimerButton.addEventListener('click', () => coutTimeTimer.onStart())
+startTimerButton.addEventListener('click', () => coutTimeTimer.onStart());
 
-const coutTimeTimer = {onStart() {setInterval(() => {
-        
-        const currentTime = Date.now();
-    const countedTime = selectedDate - currentTime;
-    const { days, hours, minutes, seconds } = convertMs(countedTime)
+const coutTimeTimer = {
+  onStart() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const countedTime = selectedDate - currentTime;
+      const { days, hours, minutes, seconds } = convertMs(countedTime);
 
-daysField.textContent = days;
-hoursField.textContent = hours;
-minutesField.textContent = minutes;
-secondsField.textContent = seconds;
-    }, 1000)
-}
-}
+      daysField.textContent = days;
+      hoursField.textContent = hours;
+      minutesField.textContent = minutes;
+      secondsField.textContent = seconds;
+
+      if (countedTime >= 0) {
+        clearInterval();
+      }
+    }, 1000);
+  },
+};
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -60,10 +64,14 @@ function convertMs(ms) {
   // Remaining hours
   const hours = String(Math.floor((ms % day) / hour)).padStart(2, '0');
   // Remaining minutes
-  const minutes = String(Math.floor(((ms % day) % hour) / minute)).padStart(2, '0');
+  const minutes = String(Math.floor(((ms % day) % hour) / minute)).padStart(
+    2,
+    '0'
+  );
   // Remaining seconds
-  const seconds = String(Math.floor((((ms % day) % hour) % minute) / second)).padStart(2, '0');
+  const seconds = String(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  ).padStart(2, '0');
 
   return { days, hours, minutes, seconds };
 }
-
